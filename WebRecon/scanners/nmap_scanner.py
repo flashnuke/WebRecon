030b42b2f66816a3114009b008c3692fa02d2c07
flashnuke <adanikel@gmail.com>
2022-07-22 19:15:45 +0300

    nmap status

--- WebRecon/scanners/nmap_scanner.py
+++ WebRecon/scanners/nmap_scanner.py
@@ -31,7 +31,7 @@ class NmapScanner(Scanner):
         self.ret_results = dict()
 
     def _start_scanner(self):
-        self._log_status(OutputStatusKeys.State, OutputValues.StateRunning)
+        self._log_status(OutputStatusKeys.State, OutputValues.StateRunning, refresh_output=True)
 
         nm = nmap.PortScanner()
         nm.scan(hosts=self.target_hostname, ports=self.ports, arguments=self.cmdline_args)
