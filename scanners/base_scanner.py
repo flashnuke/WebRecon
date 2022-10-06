8e2e4254fb293a69613b671775c2fdf2dff791eb
flashnuke <adanikel@gmail.com>
2022-10-06 10:35:32 +0300

    verify false

--- scanners/base_scanner.py
+++ scanners/base_scanner.py
@@ -310,7 +310,8 @@ class Scanner(ScanManager):
             headers = dict()
         headers.update(self._default_headers)
 
-        res = self._session.request(method=method, url=url, headers=headers, timeout=self.request_timeout, **kwargs)
+        res = self._session.request(method=method, url=url, headers=headers, timeout=self.request_timeout,
+                                    verify=False, **kwargs)
 
         if res.status_code == ScannerDefaultParams.LimitRateSCode:
             self._log_exception("too many requests", abort=False)
